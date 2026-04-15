caaa6e8a02198d2eb79704256a7db55ce56b3677
Leonel <mendiolaleonel2002@gmail.com>
2026-04-14 20:53:27 -0600

    Class

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame import mixer
+import os
+# Vamos a agregar más librerías
+
+#Background
+BACKGROUND = pygame.image.load(os.path.join('img', 'background.png'))
+ICON_IMAGE = pygame.image.load(os.path.join('img', 'title_icon.png'))
+TITLE = 'Space Invaders Hybridge'
+
+#Player
+PLAYER_IMAGE = pygame.image.load(os.path.join('img', 'player_image.png'))
+BULLET_IMAGE = pygame.image.load(os.path.join('img','bullet_image.png'))
+
+pygame.init()
+
+#Game Window
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption(TITLE)
+pygame.display.set_icon(ICON_IMAGE)
+try: 
+    mixer.music.load('sounds/background_song.mp3')
+except:
+    print("No se cargó el archivo de sonido")
+    pass
+
+def main():
+    puntaje = 0
+    run = True
+    clock = pygame.time.Clock()
+    FPS = 60
+    try:
+        mixer.music.play(-1)
+    except:
+        pass
