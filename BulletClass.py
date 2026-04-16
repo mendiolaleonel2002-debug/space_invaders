240c077f8ddcffd891e1578c247e18ef72049265
Leonel <mendiolaleonel2002@gmail.com>
2026-04-15 21:28:52 -0600

    Creamos el archivo BulletClass.py

--- BulletClass.py
+++ BulletClass.py
@@ -1,24 +1,21 @@
 import pygame
 
-#Clase Bullets (balas)
-
 class Bullet:
-    def __init__(self,x,y,img):
+    def __init__(self, x, y, img):
         self.x = x
         self.y = y
         self.img = img
         self.mask = pygame.mask.from_surface(self.img)
-
+    
     def draw(self, window):
         window.blit(self.img, (self.x, self.y))
-
+    
     def move(self, speed):
-        self.y += speed 
-
+        self.y += speed
+    
     def collision(self, obj):
-        offset = (int(self.x - obj.x), int(self.y -obj.y))
+        offset = (int(self.x - obj.x - 30), int(self.y - obj.y - 20))
         return self.mask.overlap(obj.mask, (offset))
-
 
 
 
--- ShipClass.py
+++ ShipClass.py
@@ -1,5 +1,6 @@
-#Clase Ship (Nave)
 
+
+# Clase Ship
 class Ship:
     def __init__(self, x, y, health=100):
         self.x = x
@@ -9,14 +10,15 @@ class Ship:
         self.bullet_img = None
         self.bullet_cooldown_counter = 0
         self.bullets = []
-        self.cool_down = 80
-
+        self.fired_bullets = []
+        self.cool_down = 120
+            
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
 
     def get_width(self):
         return self.ship_img.get_width()
-
+    
     def get_height(self):
         return self.ship_img.get_height()
 
